517777d81d558272e2ca591fec8eded636ce32cc
Padraig Riley <padraig.j.riley@gmail.com>
2020-04-30 16:17:10 +0200

    database_test

--- app.js
+++ app.js
@@ -15,47 +15,44 @@ app.use(bodyParser.json())
 
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
- 	ssl: { rejectUnauthorized: false }
+  	ssl: { rejectUnauthorized: false }
+})
+
+pool.connect(err => {
+	if (err) {
+		console.error('connection error', err.stack)
+	} else {
+		console.log('connected to db')
+	}	
 })
 
 app.post('/wishss', (req, res) => {
 	var wished = req.body.textarea
+	console.log('Wish: ' + wished)
 	var wish_string = "INSERT INTO wishes(wish)VALUES('"+wished+"')"
 
-	pool.connect(err => {
-	if (err) {
-		console.error('connection error', err.stack)
-		} else {
-			console.log('connected to db')
-		}	
-	})
-
 	pool.query(
-	wish_string,
-	(err, res) => {
-		console.log(err, res)
+		wish_string,
+		(err, res) => {
+			console.log(err, res)
 	}
 	)
+
 })
 
 app.post('/emailss', (req, res) => {
 	var emailed = req.body.mail
+	console.log('Email: ' + emailed)
 	var email_string = "INSERT INTO emails(email)VALUES('"+emailed+"')"
-
-	pool.connect(err => {
-	if (err) {
-		console.error('connection error', err.stack)
-		} else {
-			console.log('connected to db')
-		}	
-	})
+	console.log(email_string)
 
 	pool.query(
-	email_string,
-	(err, res) => {
-		console.log(err, res)
-	}
+		email_string,
+		(err, res) => {
+			console.log(err, res)
+		}
 	)
+
 })
 
 const port = process.env.PORT || 3000;
