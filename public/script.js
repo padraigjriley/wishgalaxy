568bd15dfca6cd4f41427f6ca213fd19c00c2958
Padraig Riley <padraig.j.riley@gmail.com>
2020-07-06 15:04:21 +0200

    text sizes

--- public/script.js
+++ public/script.js
@@ -650,7 +650,7 @@ document.getElementById('video_container').appendChild(vid_stars)
 document.getElementById('video_container').appendChild(vid_space)
 
 
-// if (isMobile){
+if (isMobile){
   // let about_text = document.getElementById('about_text')
   // let coin_text = document.getElementById('coin_text')
   let galaxy_text22 = document.getElementById('galaxy_text')
@@ -696,9 +696,9 @@ document.getElementById('video_container').appendChild(vid_space)
 
 
 
-// }else{
-//   vid_space.src="./videos/video_space.mp4"
-//   vid_stars.src="./videos/video_stars.mp4"
-// }
+}else{
+  vid_space.src="./videos/video_space.mp4"
+  vid_stars.src="./videos/video_stars.mp4"
+}
 
 
