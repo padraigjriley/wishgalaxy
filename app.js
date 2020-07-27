77d0b21c5807ca452110b2796b0f8ede68f80824
padraigjriley <64153462+padraigjriley@users.noreply.github.com>
2020-07-27 15:23:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,29 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-// Parse URL-encoded bodies (as sent by HTML forms)
-//	app.use(express.urlencoded({ extended: true}))
 
-// Parse JSON bodies (as sent by API clients)
-//app.use(express.json());
-//app.use(bodyParser.json())
-
-// const pool = new Pool({
-// 	host: 'localhost',
-// 	port: 5432,
-// 	database: 'test',
-// 	user: 'paddy',
-// 	password: '123'
-// })
-
-/*const pool = new Pool({
- 	host: 'ec2-3-222-30-53.compute-1.amazonaws.com',
- 	port: 5432,
- 	database: 'dab9u3rvn8hud1',
- 	user: 'koxpwklkrgbovt',
- 	password: 'db42652e3f4ef31807b98457ac4acae0340dea3a0deeb7ad80aeb250dca55952',
-  ssl: {rejectUnauthorized: false},
-})*/
 
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
@@ -49,21 +27,7 @@ pool.connect(err => {
 	}
 })
 
-/*app.post('/wishss', (req, res) => {
-	var wished = req.body.textarea
-	console.log('Wish: ' + wished)
-	var dt = dateTime.create()
-	dt.offsetInHours(2)
-	var now = dt.format('Y-m-d H:M:S')
-	var wish_string = "INSERT INTO wishes(wish, date)VALUES('"+wished+"', '" + now + "')"
 
-	pool.query(
-		wish_string,
-		(err, res) => {
-			console.log(err, res)
-	}
-	)
-})*/
 
 app.post('/emailss', (req, res) => {
 	var emailed = req.body.mail
@@ -115,17 +79,6 @@ io.on('connection', (socket) => {
   });
 });
 
-// app.post('/loginss', (req, res) => {
-// 	var uname = req.body.uname
-// 	var psw = req.body.psw
-// 	if ((uname == "cait") && (psw == "wendy")){
-// 		console.log("credentials correct!")
-// 		return res.redirect('admin.html');
-// 	}
-// 	else {
-// 		console.log("credentials incorrect!")
-// 	}
-// })
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log('app is listening'))
